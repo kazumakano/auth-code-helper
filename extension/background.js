79627f6db2056a4fbe7ca8db3d0de28e4df821d3
kazumakano <kazumakano@outlook.jp>
2022-10-29 19:21:19 +0900

    add extension

---
+++ extension/background.js
@@ -0,0 +1,18 @@
+const copyCode = async () => {
+  // edit here
+  const NAME = "foo"
+  const SERVER_HOST = "http://127.0.0.1"
+  const SERVER_PORT = 5000
+
+  const code = await (await fetch(`${SERVER_HOST}:${SERVER_PORT}/code/${NAME}`)).text()
+  console.log(`authentification code is ${code}`)
+
+  await window.navigator.clipboard.writeText(code)
+}
+
+chrome.action.onClicked.addListener(tab => {
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    func: copyCode
+  })
+})
