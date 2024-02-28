1e1a0c3da9fbd9d694ef3df0c003b04d958859c2
Kazuma <kazumakano@outlook.jp>
2024-02-28 21:34:39 +0900

    minor update

--- server/database.py
+++ server/database.py
@@ -4,5 +4,5 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Account(db.Model):
-    name = db.Column(db.String(), primary_key=True)
-    seed = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String, primary_key=True)
+    seed = db.Column(db.String, nullable=False)
